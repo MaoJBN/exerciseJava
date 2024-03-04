4b36f5a2a1ee59c62fcff885e4a8c8f2baab00f1
MaoJBN <mjbecerran@ufpso.edu.co>
2024-03-04 10:26:20 -0500

    project creation

---
+++ lendbook/src/main/java/com/mycompany/lendbook/Lendbook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lendbook;
+
+/**
+ *
+ * @author SCIS2PC03
+ */
+public class Lendbook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
